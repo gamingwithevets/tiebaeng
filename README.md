c9f6ab04d564db80b1ac6bd7b0cf59c99d40dc5f
gamingwithevets <thenewgwe@gmail.com>
2024-01-08 07:12:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a WIP Baidu Tieba English mod Tampermonkey script.
---
+++ script.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name Baidu Tieba English Mod
+// @namespace http://gamingwithevets.github.io
+// @version 0.1.0
+// @description  Translates Baidu Tieba to English
+// @match http://tieba.baidu.com/*
+// @copyright 2024+, GamingWithEvets Inc.
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+// unfollow button
+document.getElementById("j_head_focus_btn").style.background = "url(https://gamingwithevets.github.io/tiebaeng/img/frs_cancel_02.png) no-repeat top left";
