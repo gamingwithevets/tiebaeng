946d0047efdb59c6e1aee2189ea6b000aa1f76ab
Steveyboi <thenewgwe@gmail.com>
2024-01-11 12:48:15 +0700

    Add editor icons

--- tiebaeng.user.js
+++ tiebaeng.user.js
@@ -8,32 +8,29 @@
 // @grant        none
 // ==/UserScript==
 
-try {
-	// follow button
-	document.getElementsByClassName('islike_focus')[0].style.backgroundImage = "url(https://gamingwithevets.github.io/tiebaeng/img/frs_islike.png)";
-} catch(e) {
-	// unfollow button
-	document.getElementsByClassName('cancel_focus')[0].style.backgroundImage = "url(https://gamingwithevets.github.io/tiebaeng/img/frs_cancel_02.png)";
-}
+// follow button
+try { document.getElementsByClassName('islike_focus')[0].style.backgroundImage = "url(https://gamingwithevets.github.io/tiebaeng/img/frs_islike.png)"; } catch(e) { null; }
+// unfollow button
+try { document.getElementsByClassName('cancel_focus')[0].style.backgroundImage = "url(https://gamingwithevets.github.io/tiebaeng/img/frs_cancel_02.png)"; } catch(e) { null; }
 
-try {
-	// Guide
-	document.getElementsByClassName('post_head_official_guide')[0].style.backgroundImage = "url(https://gamingwithevets.github.io/tiebaeng/img/guide-pic.png)";
-} catch(e) { null; }
+// Guide
+try { document.getElementsByClassName('post_head_official_guide')[0].style.backgroundImage = "url(https://gamingwithevets.github.io/tiebaeng/img/guide-pic.png)"; } catch(e) { null; }
 
+// Enter Post Bar button
 try {
-	// Enter Post Bar button
 	document.getElementsByClassName('j_enter_ba')[0].innerHTML = "Enter Post Bar";
 	document.getElementsByClassName('j_enter_ba')[0].style.width = "110px";
 } catch(e) { null; }
 
+// Search All Bars button
 try {
-	// Search All Bars button
+	
 	document.getElementsByClassName('j_search_post')[0].innerHTML = "Search All Bars";
 	document.getElementsByClassName('j_search_post')[0].style.width = "110px";
 } catch(e) { null; }
 
-try {
-	// Top navigation bar
-	document.getElementsByClassName('j_search_nav')[0].style.marginRight = "700px";
-} catch(e) { null; }
+
+// Editor icons
+edui = document.getElementsByClassName('edui-icon');
+for (let i = 0; i < edui.length, i++) { edui.style.backgroundImage = "url(https://gamingwithevets.github.io/tiebaeng/img/icon_png8_old.png)"; }
+
